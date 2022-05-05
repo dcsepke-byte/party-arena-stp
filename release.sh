9b9e34440320029ad810475317e0e8d1c03a8eef
Florian Kothmeier <floriankothmeier@web.de>
2022-05-05 15:28:37 +0200

    Prepare CI for release candidate

--- release.sh
+++ release.sh
@@ -14,36 +14,47 @@ finalize_build () {
 	cd build
 	
 	zip -r ../super-tux-party-linux-64.zip plugins supertuxparty supertuxparty.pck
-	
 	zip -r ../super-tux-party-windows-64.zip plugins Supertuxparty.exe Supertuxparty.pck
+	zip -r ../super-tux-party-linux-server-64.zip plugins supertuxparty_server supertuxparty_server.pck
 	
 	mkdir tmp
 	cd tmp
 	unzip ../supertuxparty.app
-	cp -r ../plugins Super\ Tux\ Party.app/Contents/Resources/plugins
-	zip -r ../../super-tux-party-osx-64.zip Super\ Tux\ Party.app
+	cp -r ../plugins 'Super Tux Party.app/Contents/Resources/plugins'
+	zip -r ../../super-tux-party-osx-64.zip 'Super Tux Party.app'
 	cd ..
 	rm -rf tmp
 	cd ..
 }
 
+itch_deploy() {
+	butler push super-tux-party-linux-64.zip "$1:linux-64" --userversion="$2"
+	butler push super-tux-party-windows-64.zip "$1:windows-64" --userversion="$2"
+	butler push super-tux-party-osx-64.zip "$1:osx-64" --userversion="$2"
+	butler push super-tux-party-osx-64.zip "$1:server-64" --userversion="$2"
+	butler push super-tux-party-sources.zip "$1:sources" --userversion="$2"
+}
+
+website_deploy() {
+	scp super-tux-party-linux-64.zip upload@supertux.party:files/"$1"/linux.zip
+	scp super-tux-party-windows-64.zip upload@supertux.party:files/"$1"/windows.zip
+	scp super-tux-party-osx-64.zip upload@supertux.party:files/"$1"/macos.zip
+	scp super-tux-party-linux-server-64.zip upload@supertux.party:files/"$1"/server.zip
+	scp super-tux-party-sources.zip upload@supertux.party:files/"$1"/source.zip
+}
+
 finalize_build
 
-ITCH_URL=supertuxparty/super-tux-party-nightly
-
 if [ "$1" = "release" ]; then
-	ITCH_URL=anti/super-tux-party
-	curl --header "X-API-Token: $API_TOKEN"\
-		 -F "data=@super-tux-party-linux-64.zip" https://supertux.party/upload/"$2"/linux
-	curl --header "X-API-Token: $API_TOKEN"\
-		 -F "data=@super-tux-party-windows-64.zip" https://supertux.party/upload/"$2"/windows
-	curl --header "X-API-Token: $API_TOKEN"\
-		 -F "data=@super-tux-party-osx-64.zip" https://supertux.party/upload/"$2"/macos
-	curl --header "X-API-Token: $API_TOKEN"\
-		 -F "data=@super-tux-party-sources.zip" https://supertux.party/upload/"$2"/source
-fi
+	itch_deploy anti/super-tux-party "$2"
+	website_deploy "$2"
 
-butler push super-tux-party-linux-64.zip "${ITCH_URL}:linux-64" --userversion="$2"
-butler push super-tux-party-windows-64.zip "${ITCH_URL}:windows-64" --userversion="$2"
-butler push super-tux-party-osx-64.zip "${ITCH_URL}:osx-64" --userversion="$2"
-butler push super-tux-party-sources.zip "${ITCH_URL}:sources" --userversion="$2"
+	# Update the symlink for latest
+	sftp upload@supertux.party << EOF
+rm files/latest
+symlink "$2" files/latest
+EOF
+else
+	itch_deploy supertuxparty/super-tux-party-nightly "$2"
+	website_deploy nightly
+fi
