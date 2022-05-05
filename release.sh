2921cdfe3fdb2c251ea5c80c5611f2f02408d080
Florian Kothmeier <floriankothmeier@web.de>
2022-05-05 18:10:59 +0200

    Use sftp for deployment instead of scp

--- release.sh
+++ release.sh
@@ -36,11 +36,15 @@ itch_deploy() {
 }
 
 website_deploy() {
-	scp super-tux-party-linux-64.zip upload@supertux.party:files/"$1"/linux.zip
-	scp super-tux-party-windows-64.zip upload@supertux.party:files/"$1"/windows.zip
-	scp super-tux-party-osx-64.zip upload@supertux.party:files/"$1"/macos.zip
-	scp super-tux-party-linux-server-64.zip upload@supertux.party:files/"$1"/server.zip
-	scp super-tux-party-sources.zip upload@supertux.party:files/"$1"/source.zip
+	sftp upload@supertux.party:files <<EOF
+mkdir "$1"
+cd "$1"
+put super-tux-party-linux-64.zip linux.zip
+put super-tux-party-windows-64.zip windows.zip
+put super-tux-party-osx-64.zip macos.zip
+put super-tux-party-linux-server-64.zip server.zip
+put super-tux-party-sources.zip source.zip
+EOF
 }
 
 finalize_build
